865feb7cb9135f9dd0f63783f66ceeba529d64ba
Patrick-Mesey <Patrick.mesey@gmail.com>
2022-11-10 15:00:43 -0600

    setup init client folder / server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,25 @@
 //Starting point for server app
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import mongoose, { trusted } from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+//all routes will start with posts
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+
+//Variable to connect to mongodb
+const CONNECTION_URL = 'mongodb+srv://pjmes:adpbb1020@cluster0.ae01hv8.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
