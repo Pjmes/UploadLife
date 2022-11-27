8fb658a2b38c7ed19da9a6fcf3d9fd39313b3538
Patrick-Mesey <Patrick.mesey@gmail.com>
2022-11-27 16:37:22 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,10 +21,9 @@ app.get('/', (req, res) =>{
     res.send('Get Ready To Upload Your Life');
 });
 
-//4bYNrD1WVH4Z3eXX
-const CONNECTION_URL = 'mongodb+srv://pjmes:4bYNrD1WVH4Z3eXX@cluster0.ae01hv8.mongodb.net/?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
