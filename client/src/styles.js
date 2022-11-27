3aae4bc69c34ebf3389eba34dea10519f12dfa87
Patrick-Mesey <Patrick.mesey@gmail.com>
2022-11-27 17:30:10 -0600

    Adjusted form flex

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,24 +1,24 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    appBar: {
-        borderRadius: 15,
-        margin: '30px 0',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      heading: {
-        color: 'teal',
-        fontFamily: 'Cursive', 
-      },
-      image: {
-        marginLeft: '15px',
-      },
-      [theme.breakpoints.down('sm')]: {
-        mainContainer: {
+  appBar: {
+      borderRadius: 15,
+      margin: '30px 0',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    heading: {
+      color: 'teal',
+      fontFamily: 'Cursive', 
+    },
+    image: {
+      marginLeft: '15px',
+    },
+    [theme.breakpoints.down('sm')]: {
+      mainContainer: {
         flexDirection:"column-reverse"
-        },
-      }  
+      },
+    }  
 }));
